210353ae4535ffe1ecf79d275147b8add5b17c68
Tijn <me@imrat.com>
2012-05-25 12:20:06 +0100

    change to locs

---
+++ index.php
@@ -0,0 +1,6 @@
+<h1>Welcome</h1>
+<p>This is a simple pagodabox quickstart</p>
+<p>Basically it creates a webserver with a writable dir in the root.</p>
+<p>This way you can config sftp and just upload and edit your files without
+worrying about git and stuff. Great for just testing out stuff quickly</p>
+
