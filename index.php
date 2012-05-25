19681a86aa946b4c8aa560bc144c81e39910d08d
Tijn <me@imrat.com>
2012-05-25 14:23:46 +0100

    improved lp

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <h1>Welcome</h1>
 <p>This is a simple pagodabox quickstart</p>
-<p>Basically it creates a webserver with a writable dir in the root.</p>
+<p>Basically it creates a webserver with a writable for you to host webpages.</p>
 <p>This way you can config sftp and just upload and edit your files without
 worrying about git and stuff. Great for just testing out stuff quickly</p>
+<p></p>
+<h2>Writabel Dir</h2>
+<?= __DIR__ ?>/lp/<br/>
+<b>url: </b><a href="http://<?= $_SERVER['HTTP_HOST'] ?>/lp/">http://<?= $_SERVER['HTTP_HOST'] ?>/lp/</a><br/>
+
+
 
