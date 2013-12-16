8464e24d6187d12037ea4c51a2ecc06dcdc3bc40
takumi <you@example.com>
2013-12-17 01:57:15 +0900

    てすと

--- index.php
+++ index.php
@@ -8,20 +8,23 @@
 
 </head>
 <body>
+	<h1>introduction page</h1>
+
 	<img src="../web-board.png">
 	<a href="http://49.212.46.130/~g031k068/g031k068/cake/secondboards">掲示板PC版</a>
-	<br />
+	<br /><br />
 
 	<img src="../mobile-board.png">
 	<a href="http://49.212.46.130/~g031k068/g031k068/cake/secondboards">掲示板モバイル版</a>
-	<br />
+	<br /><br />
 
 	<img src="../php-board.png">
 	<a href="http://49.212.46.130/~g031k068">課題ページ</a>
-	<br />
+	<br /><br />
 
-	
+	<img src="../github.png">
 	<a href="https://github.com/sasaki-takumi">Githubページ</a>
+	<br />
 
 </body>
 </html>
