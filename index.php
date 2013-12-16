ac0e52579dffa35b59da1ad0a2c09c9234de19ca
takumi <you@example.com>
2013-12-17 01:49:41 +0900

    てすと

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 
 </head>
 <body>
+	<img src="../web-board.png">
 	<a href="http://49.212.46.130/~g031k068/g031k068/cake/secondboards">掲示板PC版</a>
-
+	<br />
 	<a href="http://49.212.46.130/~g031k068/g031k068/cake/secondboards">掲示板モバイル版</a>
 	<a href="http://49.212.46.130/~g031k068">課題ページ</a>
 	<a href="https://github.com/sasaki-takumi">Githubページ</a>
