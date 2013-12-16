3187fe670954bdcf01ea4050a9cd5252f326031c
takumi <you@example.com>
2013-12-17 01:53:16 +0900

    てすと

--- index.php
+++ index.php
@@ -11,8 +11,16 @@
 	<img src="../web-board.png">
 	<a href="http://49.212.46.130/~g031k068/g031k068/cake/secondboards">掲示板PC版</a>
 	<br />
+
+	<img src="../mobile-board.png">
 	<a href="http://49.212.46.130/~g031k068/g031k068/cake/secondboards">掲示板モバイル版</a>
+	<br />
+
+	<img src="../php-board.png">
 	<a href="http://49.212.46.130/~g031k068">課題ページ</a>
+	<br />
+
+	
 	<a href="https://github.com/sasaki-takumi">Githubページ</a>
 
 </body>
