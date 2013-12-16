143290e5ac646df7226c5fc78c7a13e5d9b429b3
takumi <you@example.com>
2013-12-17 01:46:35 +0900

    てすと

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<h1>Welcome</h1>
-<p>This is a simple pagodabox quickstart</p>
-<p>Basically it creates a webserver with a writable for you to host webpages.</p>
-<p>This way you can config sftp and just upload and edit your files without
-worrying about git and stuff. Great for just testing out stuff quickly</p>
-<p></p>
-<h2>Writabel Dir</h2>
-<?= __DIR__ ?>/lp/<br/>
-<b>url: </b><a href="http://<?= $_SERVER['HTTP_HOST'] ?>/lp/">http://<?= $_SERVER['HTTP_HOST'] ?>/lp/</a><br/>
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
 
+	<LINK href="../intro.css" rel="stylesheet" type="text/css">
 
+</head>
+<body>
+	<a href="http://49.212.46.130/~g031k068/g031k068/cake/secondboards">掲示板PC版</a>
 
+	<a href="http://49.212.46.130/~g031k068/g031k068/cake/secondboards">掲示板モバイル版</a>
+	<a href="http://49.212.46.130/~g031k068">課題ページ</a>
+	<a href="https://github.com/sasaki-takumi">Githubページ</a>
+
+</body>
+</html>
